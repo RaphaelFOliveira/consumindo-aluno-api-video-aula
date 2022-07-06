fc07e29921185070a32f37f2c41bc28cff33baf8
Raphael Fernandes Oliveira <482raphael@gmail.com>
2022-07-06 19:04:37 -0300

    Aplicacao consome api_aluno

---
+++ ConsumindoApiAluno/Entities/Services/AlunoServices.cs
@@ -0,0 +1,33 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsumindoApiAluno.Entities.Services
+{
+    internal class AlunoServices
+    {
+        public async Task<Aluno> Integracao(int id)
+        {
+            HttpClient httpClient = new HttpClient();
+            var response = await httpClient.GetAsync($"https://localhost:7141/aluno/{id}");
+            var jsonString = await response.Content.ReadAsStringAsync();
+
+            Aluno jsonObject = JsonConvert.DeserializeObject<Aluno>(jsonString);
+
+            if(jsonObject != null)
+            {
+                return jsonObject;
+            }
+            else
+            {
+                return new Aluno
+                {
+                    Vericacao = true
+                };
+            }
+        }
+    }
+}
